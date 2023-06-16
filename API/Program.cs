364aafeeaccf6da79e5a187119d68ea3fcfb40ef
Murat Muldashev <97629199+muldashev23@users.noreply.github.com>
2023-06-16 02:55:22 +0200

    Cors Policy created in cs file and connected our API to angular client

--- API/Program.cs
+++ API/Program.cs
@@ -10,10 +10,10 @@ builder.Services.AddDbContext<DataContext>(options=>
 {
     options.UseMySql(connectionString,ServerVersion.AutoDetect(connectionString));
 });
-
+builder.Services.AddCors();
 var app = builder.Build();
 
-
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 app.MapControllers();
 
 app.Run();
