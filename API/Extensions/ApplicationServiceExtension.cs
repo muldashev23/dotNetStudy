0482deadb2de84fa32877b872b641ccf4642299a
Murat Muldashev <97629199+muldashev23@users.noreply.github.com>
2023-06-17 17:58:31 +0200

    Section 4 done

---
+++ API/Extensions/ApplicationServiceExtension.cs
@@ -0,0 +1,23 @@
+﻿using API.Data;
+using API.Intefaces;
+using API.Services;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Extensions
+{
+    public static class ApplicationServiceExtension
+    {
+        public static IServiceCollection AddAplicationServices(this IServiceCollection services, IConfiguration config)
+        {   
+            var connectionString = config.GetConnectionString("DefaultConnection");
+            services.AddDbContext<DataContext>(options=>
+            {
+                options.UseMySql(connectionString,ServerVersion.AutoDetect(connectionString));
+            });
+            services.AddCors();
+            services.AddScoped<ITokenService, TokenService>();
+            return services;
+        }
+    }   
+
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,32 +1,14 @@
 using System.Text;
-using API.Data;
-using API.Intefaces;
-using API.Services;
+using API.Entities;
+using API.Extensions;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<DataContext>(options=>
-{
-    options.UseMySql(connectionString,ServerVersion.AutoDetect(connectionString));
-});
-builder.Services.AddCors();
-builder.Services.AddScoped<ITokenService, TokenService>();
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options=>
-{
-    options.TokenValidationParameters = new TokenValidationParameters
-    {
-        ValidateIssuerSigningKey = true,
-        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["TokenKey"])), 
-        ValidateIssuer = false,
-        ValidateAudience = false
-    };
-});
+builder.Services.AddAplicationServices(builder.Configuration);
+builder.Services.AddIdentityServices(builder.Configuration);
 var app = builder.Build();
 
 app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
